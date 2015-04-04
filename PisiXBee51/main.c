5db01b00155e673e8948b6756e5e434108497131
Kristjan Roosild <kristjan.r@gmail.com>
2015-04-04 17:44:58 +0300

    Correct side and diagonal sensors (pairs should give equal data in equal condition) Correct motor speed to drive straight

--- PisiXBee51/main.c
+++ PisiXBee51/main.c
@@ -11,120 +11,168 @@
 #include "drivers/motor.h"
 #include "drivers/com.h"
 #include "drivers/gyro.h"
-#define Turn_time 350 //for 90 degrees turn
-int front_left_sens = 0;
-int left_sens = 1;
-int left_diag_sens = 4;
 
-int front_right_sens= 3;
-int right_sens = 5;
-int right_diag_sens = 2;
-			
+#define TURN_TIME 350 // for 90 degrees turn
+#define TURN_SPEED 1000 //for 90 degrees turn
+#define MOVE_SPEED 900 // not 1000 so could add speed for course correction
+
+#define FRONT_LEFT_S 0
+#define LEFT_S 1
+#define LEFT_DIAG_S 4
+
+#define FRONT_RIGHT_S 3
+#define RIGHT_S  5
+#define RIGHT_DIAG_S 2
+
+int get_left_diag();
+
+int get_right_diag();
+
+void set_motor();
+
+void go();
+
 void delay_ms(uint16_t count) {
-	while(count--) {
-		_delay_ms(1);
+    while (count--) {
+        _delay_ms(1);
 
-	}
+    }
+}
+
+int get_front_right() {
+    return adc_read(FRONT_RIGHT_S);
+}
+
+int get_left_right() {
+    return adc_read(FRONT_LEFT_S);
+}
+
+int get_left() {
+    return adc_read(LEFT_S);
+}
+
+int get_right() {
+    return adc_read(RIGHT_S) - 2;
+}
+
+int get_right_diag() {
+    // correction. The average difference is that left diag sens shows 2,57 more
+    return adc_read(RIGHT_DIAG_S) + 3;
+}
+
+int get_left_diag() { return adc_read(LEFT_DIAG_S); }
+
+void send_debug_msg() {
+    char buff[30];
+    int right_diag = adc_read(RIGHT_DIAG_S);
+    int left_diag = adc_read(LEFT_DIAG_S);
+    int diff = left_diag - right_diag;
+    sprintf(buff, "%4d,%4d,%4d,%4d,%4d \n\r", left_diag, right_diag, diff, get_left(), get_right());
+    radio_puts(buff);
 }
 
 void turn_right_and_back() {
-	int turn_time = 350;
-	int16_t turn_speed = 1000;
-	motor_left_set(turn_speed);
-	delay_ms(turn_time);
-	motor_left_set(0);
-	delay_ms(turn_time);
-	motor_left_set(-turn_speed);
-	delay_ms(turn_time);
-	motor_left_set(0);
+    motor_left_set(TURN_SPEED);
+    delay_ms(TURN_TIME);
+    motor_left_set(0);
+    delay_ms(TURN_TIME);
+    motor_left_set(-TURN_SPEED);
+    delay_ms(TURN_TIME);
+    motor_left_set(0);
 
 }
 
 void turn_left_and_back() {
-	int16_t turn_speed = 1000;
-	motor_right_set(turn_speed);
-	delay_ms(Turn_time);
-	motor_right_set(0);
-	delay_ms(Turn_time);
-	motor_right_set(-turn_speed);
-	delay_ms(Turn_time);
-	motor_right_set(0);
+    motor_right_set(TURN_SPEED);
+    delay_ms(TURN_TIME);
+    motor_right_set(0);
+    delay_ms(TURN_TIME);
+    motor_right_set(-TURN_SPEED);
+    delay_ms(TURN_TIME);
+    motor_right_set(0);
 }
 
 void turn_around(int right) {
-	int16_t turn_speed = 1000;
-	if (right) {
-		motor_set(turn_speed, -turn_speed);
-		} else {
-		motor_set(-turn_speed, turn_speed);
-	}
-	delay_ms(350);
-	motor_set(0, 0);
-	
+    if (right) {
+        motor_set(TURN_SPEED, -TURN_SPEED);
+    } else {
+        motor_set(-TURN_SPEED, TURN_SPEED);
+    }
+    delay_ms(TURN_TIME);
+    motor_set(0, 0);
+
+}
+
+void motors(int16_t l_speed, int16_t r_speed) {
+    // correct the slight curving to right
+    int16_t left = l_speed - 25;
+    motor_set(left, r_speed);
+}
+
+void forward() {
+    motors(MOVE_SPEED, MOVE_SPEED);
 }
 
 void straight(int move_time) {
-	int16_t move_speed = 700;
-	int count = move_time;
-	while (count) {
-		count -= 10;
-		motor_set(move_speed, move_speed);
-		delay_ms(10);
-		int error = adc_read(left_diag_sens) - adc_read(right_diag_sens);
-		int right = move_speed + error;
-		int left = move_speed - error;
-		motor_set(left,right);
-	}
-	motor_set(0, 0);
+    int count = move_time;
+    int faster = MOVE_SPEED + 100;
+    int slower = MOVE_SPEED - 100;
+    while (count and get_front_left() < 100 && get_front_right() < 100) {
+        int diag_diff = get_left_diag() - get_right_diag();
+        if (diag_diff > 10 && get_right() > 35) {
+            motors(faster, slower);
+        } else if (diag_diff < -10 && get_left() > 35) {
+            motors(slower, faster);
+        } else {
+            forward();
+        }
+        count -= 10;
+        delay_ms(10);
+    }
+    motor_set(0, 0);
 }
 
-void send_debug_msg(char * buff) {
-	int difference = adc_read(left_diag_sens) - adc_read(right_diag_sens);
-	sprintf(buff,"%4d,%4d,%4d,%4d,%4d,%4d,%4d\n\r",
-	adc_read(front_left_sens),adc_read(left_sens),adc_read(left_diag_sens),
-	adc_read(front_right_sens), adc_read(right_sens), adc_read(right_diag_sens),
-	difference);
-	radio_puts(buff);
-}
-
-int main(void)
-{
-	char buff[30];
-	clock_init();		// Seadista s?steemi kell 32MHz peale
-	board_init();		// Seadista LED ja nupud
-	adc_init();			// Seadista ADC kanal 0
-	radio_init(57600);	// Seadista raadiomooduli UART
-	motor_init();		// Seadista mootorikontroller
-	
-	while(1)
-	{
-		if(sw1_read())
-		{
-			rgb_set(RED);
-			delay_ms(1000);
-			turn_right_and_back();
-			delay_ms(1000);
-			turn_left_and_back();
-			delay_ms(1000);
-			turn_around(1);
-			delay_ms(1000);
-			turn_around(0);
-
-		}
-		else if(sw2_read())
-		{
-			rgb_set(BLUE);
-			delay_ms(1000);
-			straight(5000);
-			delay_ms(1000);
-		}
-		else
-		{
-			rgb_set(GREEN);
-			send_debug_msg(buff);
-
+int main(void) {
+    clock_init();        // Seadista systeemi kell 32MHz peale
+    board_init();        // Seadista LED ja nupud
+    adc_init();            // Seadista ADC kanal 0
+    radio_init(57600);    // Seadista raadiomooduli UART
+    motor_init();        // Seadista mootorikontroller
+    bool go = true;
+    while (go) {
+        if (sw2_read()) {
+            rgb_set(RED);
+            delay_ms(1000);
+            while (!sw1_read())
+                go();
+        }
+        else if (sw1_read()) {
+            rgb_set(BLUE);
+            go = false;
+        }
+        else {
+            rgb_set(GREEN);
+            send_debug_msg();
             _delay_ms(500);
-		}
+        }
+    }
+}
+void drive_left() {
+    motors(MOVE_SPEED - 500, MOVE_SPEED);
+    _delay_ms(1000);
+    forward();
 
-	}
+}void drive_right() {
+    motors(MOVE_SPEED, MOVE_SPEED - 500);
+    _delay_ms(100);
+    forward();
+}
+
+void go() {
+    straight(50);
+    if (get_left() < 35) {
+        drive_left();
+    } else if (get_right() < 35) {
+        drive_right();
+    }
 }
